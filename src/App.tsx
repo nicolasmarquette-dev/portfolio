78d16c756c495f32c7c78de7e462c08e2ac853fd
Nicolas Marquette <nicolasmarquette.dev@gmail.com>
2024-10-19 13:29:21 +0200

    Marquette Portfolio

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { NavBar } from "./components/navbar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.tsx
@@ -0,0 +1,3 @@
+export const NavBar = (): JSX.Element => {
+  return <div className="bg-gray-200">hello</div>;
+};
