0dc199ce65897ffbc61f8ae0c583d7dc4b17bb2f
Nicolas Marquette <nicolasmarquette.dev@gmail.com>
2024-10-20 17:05:22 +0200

    Overflow hidden

--- src/App.tsx
+++ src/App.tsx
@@ -4,43 +4,12 @@ import { Presentation } from "./components/presentation/presentation";
 
 function App() {
   return (
-    <div className="w-full min-h-screen bg-[#0b0918] font-roboto">
+    <div className="w-full min-h-screen bg-[#0b0918] font-roboto ">
       <NavBar />
-      <div className="flex-grow overflow-y-auto">
+      <div className="flex-grow overflow-hidden">
         <Presentation />
         {/* Ajoute ton contenu ici */}
-        <div>
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-        </div>
+        <div></div>
       </div>
     </div>
   );
